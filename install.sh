3be8182104eafc6a4dade6290e30abb09e0ad05e
Daniel Kennedy <daniel.kennedy.m1@gmail.com>
2022-11-29 05:59:55 -0500

    Migrating to better install procedure

--- install.sh
+++ install.sh
@@ -1,189 +1,81 @@
 #!/usr/bin/env bash
-ROOT_DIR="$(dirname "$(readlink -f "$0")")"
-CONFIG_DIR="$ROOT_DIR/config"
-SCRIPTS_DIR="$ROOT_DIR/scripts"
-SRC_DIR="$ROOT_DIR/src"
-VERSION=$(cat "$ROOT_DIR/VERSION")
-WSL=$(if grep -q microsoft /proc/version; then echo 'true'; else echo 'false'; fi)
-if [ -z ${NXTOSEK+x} ]; then
-    export NXTOSEK="/usr/local/src/nxtosek"
-fi
 
-# Prerequisites
-sudo apt-get update
-sudo apt-get install wget curl git
-cd "$SCRIPTS_DIR"
-chmod +x *.sh
+# ***************************************
+# FUNCTIONS AND VARIABLES
+# ***************************************
+root_dir="$(dirname "$(readlink -f "$0")")"
+script_dir="$root_dir/scripts"
+src_dir="$root_dir/src"
+version=$(cat "$root_dir/VERSION")
 
-# Download latest version of WSL-USB scripts
-echo "Downloading repository: https://github.com/daniel-scripts/wsl-usb-scripts.git"
-if [ ! -d "$SCRIPTS_DIR/wsl-usb-scripts" ]; then
-    cd "$SCRIPTS_DIR"
-    git clone https://github.com/danielk-98/wsl-usb-scripts.git
-else
-    cd "$SCRIPTS_DIR/wsl-usb-scripts"
-    git pull origin main
-fi
-cd "$SCRIPTS_DIR/wsl-usb-scripts"
-chmod +x *.sh
-
-# Download latest version of Docker scripts
-echo "Downloading repository: https://github.com/daniel-scripts/docker-scripts.git"
-if [ ! -d "$SCRIPTS_DIR/docker-scripts" ]; then
-    cd "$SCRIPTS_DIR"
-    git clone https://github.com/danielk-98/docker-scripts.git
-else
-    cd "$SCRIPTS_DIR/docker-scripts"
-    git pull origin main
-fi
-cd "$SCRIPTS_DIR/docker-scripts"
-chmod +x *.sh
-
-# UDEV
-echo
-echo "${bold}UDEV${normal}"
-echo
-echo "UDEV rules are required to access NXT/SAM-BA devices over USB."
-echo "Without this, NXT tools will not work without 'sudo' and USB-passthrough to Docker will not be available."
-read -r -p "${bold}Install UDEV rules? (Y/N): ${normal} " 
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    echo "Adding UDEV rules for NXT devices..."
-    sudo groupadd -f plugdev
-    sudo groupadd -f dialout
-    sudo groupadd -f users
-    sudo usermod -aG plugdev $USER
-    sudo usermod -aG dialout $USER
-    sudo usermod -aG users $USER
-    cd "$CONFIG_DIR/udev"
-    sudo cp -f "./70-nxt.rules" "/etc/udev/rules.d/70-nxt.rules"
-    sudo cp -f "./nxt_event_handler.sh" "/etc/udev/nxt_event_handler.sh" && sudo chmod +x "/etc/udev/nxt_event_handler.sh"
-    sudo udevadm control --reload-rules && sudo udevadm trigger
-fi
-
-# WSL only: Install USB-IP services for Linux
-if [ $WSL == 'true' ]; then
-    echo
-    echo "${bold}USB-IP${normal}"
-    echo
-    echo "WSL2 requires USB-over-IP to connect to the host machine's USB devices."
-    echo "Make sure you also have the latest release of USBIPD Server installed on Windows as well: "
-    echo "https://github.com/dorssel/usbipd-win/releases/"
-    read -r -p "${bold}Install USB-IP Services? (Y/N): ${normal} " 
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-    
-        if [ ! -f "/etc/default/usbip-automount" ]; then
-            echo "Installing USB-IP for WSL..."
-            cd "$SCRIPTS_DIR/wsl-usb-scripts"
-            "./install.sh"
-        else
-            echo "It appears wsl-usb-scripts has already been installed. Skipping..."
-        fi
-
-        echo "Setting NXT devices to autoattach to WSL"
-        APPEND='DEVICE_MATCH_SUBSTRINGS=$(echo -e "$DEVICE_MATCH_SUBSTRINGS\n0694:0002\n03eb:6124")'
-        FILE="/etc/default/usbip-automount"
-        sudo grep -qxF "$APPEND" "$FILE" || echo "$APPEND" | sudo tee -a "$FILE" > /dev/null
-
-        service usbip-automount stop
-        sleep 2
-        service usbip-automount start
-    fi
-fi
-
-# Add bluetooth support
-echo
-echo "${bold}Bluetooth${normal}"
-echo
-read -r -p "${bold}Install Bluetooth services? (Y/N): ${normal} " 
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    cd "$SCRIPTS_DIR"
-    sudo apt-get install -y --no-install-recommends \
-        bluez bluetooth
-fi
-
-# Install Docker
-echo
-echo "${bold}Docker${normal}"
-echo
-echo "Docker is required to use the nxtOSEK Docker Image."
-echo "Docker is also required to compile LibNXT, without which some nxtOSEK tools will not function."
-echo "If not installed, "
-echo "all toolchain components (Wine, ARM cross-compiler, etc. will need to be installed on your system."
-read -r -p "${bold}Install Docker? (Y/N): ${normal} " 
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    if [ ! -x "$(command -v docker)" ]; then
-        echo "Docker not detected. Installing now..."
-        cd "$SCRIPTS_DIR/docker-scripts"
-        "./install-docker.sh"
+# Import functions from other files
+sources=(   "$script_dir/bash-common-scripts/common-functions.sh" 
+            "$script_dir/bash-common-scripts/common-io.sh"
+            "$script_dir/bash-common-scripts/common-sysconfig.sh"
+            "$script_dir/bash-common-scripts/wsl-functions.sh"
+            "$script_dir/installation-routines.sh"                     )
+for i in "${sources[@]}"; do
+    if [ ! -e "$i" ]; then
+        echo "Error - could not find required source: $i"
+        echo "Please run:"
+        echo "  git submodule update --init --recursive --remote"
+        echo ""
+        exit 1
     else
-        echo "It appears Docker has already been installed. Skipping..."
+        source "$i"
     fi
+done
 
-    # This setting is required for Wine to run within Docker
-    echo "Setting SYSCTL parameters..."
-    sudo sysctl -w vm.mmap_min_addr=0
-fi
 
-# NXT Tools (Local Install)
-echo
-echo "${bold}NXT Tools (Local Install)${normal}"
-echo
-echo "This will download, build, and install to /usr/local/bin: "
-echo " 1. John Hansen's NeXTTool (1.2.1.r5) from BricxCC 3.0"
-echo "    - Required for downloading .rxe binaries to the NXT"
-echo "    - Required for downloading .rfw firmware to the NXT"
-echo " 2. Roman Shaposhnik's LibNXT (0.3) (requires Docker)"
-echo "    - Optional for downloading .rfw firmware to the NXT"
-echo "    - Required for rambooting programs on the NXT"
-echo "If not installed, they can still be used within the Docker image."
-read -r -p "${bold}Install NXT Tools? (Y/N): ${normal} " 
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    cd "$SCRIPTS_DIR"
-    "./install_nxt_tools.sh"
+# ***************************************
+# ARGS
+# ***************************************
+declare -a args=( [skip-all]=false )
+fast_argparse args "" "skip-prompts"
+
+if [[ "${args[skip-prompts]}" == true ]]; then
+    _AUTOCONFIRM=true
 fi
 
 
-# nxtOSEK Toolchain (Local Install)
-echo
-echo "${bold}nxtOSEK Toolchain (Local Install)${normal}"
-echo "This is the recommended way to use nxtOSEK."
-echo "Will download and install the following software required for nxtOSEK: "
-echo " 1. ARM-None-EABI (ARM cross compiler)"
-echo " 1. GDB, OpenOCD, FTDI-support (real-time debugging utilities)"
-echo " 2. Wine-stable (required during linking process)"
-echo " 3. nxtOSEK development files (installs to $NXTOSEK)"
-echo "If not installed, you can choose to install the Docker image instead."
-read -r -p "${bold}Install nxtOSEK? (Y/N): ${normal} " 
-echo
-if [[ $REPLY =~ ^[Yy]$ ]]; then
-    cd "$SCRIPTS_DIR"
-    "./install_toolchain.sh"
-fi
+# ***************************************
+# SCRIPT START
+# ***************************************
+require_non_root
 
-# nxtOSEK Docker Image
-if [ -x "$(command -v docker)" ]; then
-    echo
-    echo "${bold}nxtOSEK Docker Image${normal}"
-    echo
-    echo "This downloads a precompiled image containing a complete nxtOSEK development environment and all required NXT tools."
-    echo "This may take some time."
-    read -r -p "${bold}Install nxtOSEK Docker Image? (Y/N): ${normal} " 
-    echo
-    if [[ $REPLY =~ ^[Yy]$ ]]; then
-        cd "$SCRIPTS_DIR"
-        "./download_release.sh"
-    fi
-else
-    echo
-    echo "Skipping option to download nxtOSEK Docker Image."
-    echo " (Docker not installed.)"
-fi
+# Main Menu
+title="NXTOSEK $version Installation Procedure"
+description="""\
+The following steps are required.
+Please run them in order. \
+"""
+unset options
+unset fncalls
+declare -A options
+declare -A fncalls; 
+options[1]="Install prerequisites"
+fncalls[1]="install_prerequisites"
+options[2]="Install Files"
+fncalls[2]="install_files"
+options[3]="Enable Services"
+fncalls[3]="enable_services"
 
-echo "nxtOSEK Installer is complete."
-echo "Please restart your machine before continuing."
+function_select_menu options fncalls "$title" "$description"
+
+echo "
+The following scripts have been installed in \"$INSTALL_DIR\":
+  usbip-list
+  usbip-attach
+  usbip-detach
+
+Make sure you have downloaded and installed the latest version of USB-IP on Windows:
+  https://github.com/dorssel/usbipd-win/releases
+
+Please edit the config file to choose what devices to auto-attach to WSL:
+  sudo nano /etc/default/usbip-automount
+
+Then, restart your machine. Both 'udev' and 'usbip-automount' services should now start automatically.
+"
+
+# ***************************************
 
---
+++ scripts/installation-routines.sh
@@ -0,0 +1,229 @@
+
+# Prereqs
+function install_prerequisites() {
+    local -a packages=(
+        wget curl unzip git gnupg software-properties-common python3 vim
+    )
+    echo ""
+    echo "The following APT packages will be installed:" 
+    echo "  ${packages[@]}"
+    echo ""
+    if ! confirmation_prompt; then return; fi;
+    echo ""
+
+    echo "Installing prerequisites..."
+    sudo apt-get update
+    sudo apt-get install -y ${packages[@]}
+}
+
+
+# Download latest version of WSL-USB scripts
+function install_wsl_usb() {
+    if ! is_wsl; then
+        echo "This option is only available on WSL2."
+        return
+    fi
+    local url="https://github.com/daniel-utilities/wsl-usb-scripts.git"
+    local branch=main
+    local name="$(basename $url)"; name="${name%.*}"
+    local tmp_dir="/tmp"
+    local repo_dir="$tmp_dir/$name"
+    local install_cmd="install.sh --skip-prompts true"
+    local original_dir="$pwd"
+    echo ""
+    echo "WSL2 does not come with USB pass through by default."
+    echo "An additional service (usbipd) is needed on both Windows and WSL"
+    echo "to connect USB devices automatically."
+    echo ""
+    echo "The following repository will be downloaded into $repo_dir:" 
+    echo "  URL=$url"
+    echo "  BRANCH=$branch"
+    echo ""
+    echo "Then, the following script will be run:"
+    echo "  $install_cmd"
+    echo ""
+    if ! confirmation_prompt; then return; fi;
+    echo ""
+
+    echo "Downloading repository: $name"
+    cd "$tmp_dir"
+    git_latest "$url" "$branch"
+    cd "$repo_dir"
+
+    echo "Running command: $install_cmd"
+    bash "./$install_cmd"
+    cd "$original_dir"
+
+    rm -rf "$repo_dir"
+}
+
+
+# Download and run latest version of Docker scripts
+function install_docker() {
+    if [ -x "$(command -v docker)" ]; then
+        echo "Docker is already installed."
+        if ! confirmation_prompt; then return; fi;
+    fi
+    local url="https://github.com/daniel-utilities/docker-scripts.git"
+    local branch=main
+    local name="$(basename $url)"; name="${name%.*}"
+    local tmp_dir="/tmp"
+    local repo_dir="$tmp_dir/$name"
+    local install_cmd="install.sh --skip-prompts true"
+    local original_dir="$pwd"
+    echo ""
+    echo "The following repository will be downloaded into $repo_dir:" 
+    echo "  URL=$url"
+    echo "  BRANCH=$branch"
+    echo ""
+    echo "Then, the following script will be run:"
+    echo "  $install_cmd"
+    echo ""
+    if ! confirmation_prompt; then return; fi;
+    echo ""
+
+    echo "Downloading repository: $name"
+    cd "$tmp_dir"
+    git_latest "$url" "$branch"
+    cd "$repo_dir"
+
+    echo "Running command: $install_cmd"
+    bash "./$install_cmd"
+    cd "$original_dir"
+
+    # This setting is required for Wine to run within Docker
+    echo "Setting SYSCTL parameters..."
+    sudo sysctl -w vm.mmap_min_addr=0
+
+    rm -rf "$repo_dir"
+}
+
+
+# NXT Tools
+function install_nxt_tools() {
+    local url="https://github.com/daniel-utilities/mindstorms-scripts.git"
+    local branch=main
+    local name="$(basename $url)"; name="${name%.*}"
+    local tmp_dir="/tmp"
+    local repo_dir="$tmp_dir/$name"
+    local install_cmd="install_nxt_tools.sh --skip-prompts true"
+    local original_dir="$pwd"
+    echo ""
+    echo "This will download, build, and install to /usr/local/bin: "
+    echo " 1. John Hansen's NeXTTool (1.2.1.r5) from BricxCC 3.0"
+    echo "    - Required for downloading .rxe binaries to the NXT"
+    echo "    - Required for downloading .rfw firmware to the NXT"
+    echo " 2. Roman Shaposhnik's LibNXT (0.3) (requires Docker)"
+    echo "    - Optional for downloading .rfw firmware to the NXT"
+    echo "    - Required for rambooting programs on the NXT"
+    echo ""
+    echo "The following repository will be downloaded into $repo_dir:" 
+    echo "  URL=$url"
+    echo "  BRANCH=$branch"
+    echo ""
+    echo "Then, the following script will be run:"
+    echo "  $install_cmd"
+    echo ""
+    if ! confirmation_prompt; then return; fi;
+    echo ""
+
+    echo "Downloading repository: $name"
+    cd "$tmp_dir"
+    git_latest "$url" "$branch"
+    cd "$repo_dir"
+
+    echo "Running command: $install_cmd"
+    bash "./$install_cmd"
+    cd "$original_dir"
+
+    rm -rf "$repo_dir"
+}
+
+
+# BLUETOOTH
+function install_bluetooth_services() {
+    local -a packages=(
+        bluetooth bluez
+    )
+    echo ""
+    echo "Bluetooth support allows remote control of the NXT"
+    echo "and remote program download."
+    echo ""
+    echo "The following APT packages will be installed:" 
+    echo "  ${packages[@]}"
+    echo ""
+    if ! confirmation_prompt; then return; fi;
+    echo ""
+
+    echo "Installing Bluetooth support..."
+    sudo apt-get update
+    sudo apt-get install -y --no-install-recommends ${packages[@]}
+}
+
+
+# NXTOSEK
+function install_toolchain() {
+    local nxtosek_src="$root_dir/nxtosek"
+    local install_dir="/usr/local/src"
+    local -a install_files=(
+        "$script_dir/wine_headless.sh : /usr/local/bin/wine_headless"
+    )
+    local -a compiler_packages=(     # Install ARM cross-compiler
+        make gcc-arm-none-eabi binutils-arm-none-eabi libnewlib-arm-none-eabi libstdc++-arm-none-eabi-newlib 
+    )
+    local -a debugger_packages=(     # Install JTAG debugger support
+        usbutils gdb-multiarch openocd libusb-0.1-4 libusb-dev libftdi1 libftdi-dev
+    )
+    local -a wine_packages=(
+        xvfb winehq-stable
+    )
+    echo ""
+    echo "The NXTOSEK Toolchain consists of the following components:"
+    echo " 1. nxtOSEK development files (installs to $install_dir/nxtosek)"
+    echo " 2. ARM-None-EABI (ARM cross compiler)"
+    echo " 3. C Standard Library for ARM (newlib)"
+    echo " 4. GDB, OpenOCD, FTDI-support (real-time debugging utilities)"
+    echo " 5. Wine-stable (required during linking process)"
+    echo ""
+    echo "The following APT packages will be installed:" 
+    echo "  ${compiler_packages[@]} ${debugger_packages[@]} ${wine_packages[@}]}"
+    echo "The following files will be installed:" 
+    print_arr install_files
+    echo "The following folder will be created:"
+    echo "  $install_dir"
+    echo ""
+    if ! confirmation_prompt; then return; fi;
+    echo ""
+
+    echo "Copying files..."
+    multicopy install_files
+
+    echo "Installing nxtOSEK to $install_dir..."
+    sudo rm -rf "$install_dir/nxtosek"
+    sudo cp -rf "$nxtosek_src" "$install_dir"
+
+    echo "Installing ARM cross-compiler and debugger..."
+    sudo apt-get update
+    sudo apt-get -y install --install-recommends ${compiler_packages[@]}
+    sudo apt-get -y install --no-install-recommends ${debugger_packages[@]}
+
+    echo "Installing Wine..."
+    if [ -x "$(command -v wine)" ]; then
+        echo "Wine is already installed."
+        if ! confirmation_prompt; then return; fi;
+    fi
+    sudo dpkg --add-architecture i386
+    wget -nc https://dl.winehq.org/wine-builds/winehq.key
+    sudo apt-key add winehq.key
+    rm winehq.key
+    sudo apt-add-repository https://dl.winehq.org/wine-builds/ubuntu
+    sudo apt-get update
+    sudo apt-get -y --no-install-recommends install ${packages[@]}
+
+    echo "Initializing Wine..."
+    export WINEDLLOVERRIDES="mscoree,mshtml="
+    wine-headless wineboot
+}
+
+
+
